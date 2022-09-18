650661d176615ca5dc55e24cad93420b153b406f
RonaldRommel <ronaldrommel01@gmail.com>
2022-09-18 22:59:58 +0530

    marks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-
+var cookieParser = require("cookie-parser");
 // const Blog = require("./models/blog");
 // const blogRoutes = require("./routes/blogRoutes");
 // const blogController = require("./controllers/blogController");
@@ -13,6 +13,7 @@ console.log("listening on port 3000 ");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("homepage", { title: "home" });
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 app.get("/signup", (req, res) => {
   res.render("signup", { title: "signup" });
 });
+
 app.get("/login", (req, res) => {
   res.render("login", { title: "login" });
 });
@@ -30,8 +32,120 @@ app.get("/semester", (req, res) => {
 });
 
 app.get("/semester/marks", (req, res) => {
-  res.render("marks", { title: "marks" });
+  var subjects = req.cookies["subjects"];
+  var sem = subjects[0];
+  subjects = subjects.slice(1)[0];
+  console.log(sem);
+  console.log("Subjects :", subjects);
+  // res.clearCookie("subjects", { httpOnly: true });
+  res.render("marks", { title: "marks", subjects: subjects, sem: sem });
 });
+
+// ------------------------------------------------- semesters ----------------------------------------------------------
+app.get("/semester/semc", (req, res) => {
+  var subs = [
+    "18MAT11/21",
+    "18CHE12/22",
+    "18CPS13/23",
+    "18ELN14/24",
+    "18ME15/25",
+    "18CHEL16/26",
+    "18CPL17/27",
+    "18EGH18/28(ENGLISH)",
+  ];
+  console.log("called");
+  res.cookie("subjects", ["1st and 2nd Sem (chemistry group)", subs], {
+    httpOnly: true,
+  });
+  res.redirect("/semester/marks");
+});
+
+app.get("/semester/semp", (req, res) => {
+  var subs = [
+    "18MAT11/21",
+    "18PHY12/22",
+    "18ELE13/23",
+    "18CIV14/24",
+    "18EGDL15/25",
+    "18PHYL16/26",
+    "18ELEL17/27",
+    "18EGH18/28(ENGLISH)",
+  ];
+  res.cookie("subjects", ["1st and 2nd Sem (Physics Group)", subs], {
+    httpOnly: true,
+  });
+  res.redirect("/semester/marks");
+});
+
+app.get("/semester/sem3o4", (req, res) => {
+  var subs = [
+    "18MAT31/41",
+    "18**32/42",
+    "18**33/43",
+    "18**34/44",
+    "18**35/45",
+    "18**36/46",
+    "18**L37/47",
+    "18**L38/48",
+    "18CPC39/49 or Kannada",
+  ];
+  res.cookie("subjects", ["3rd or 4th Semester", subs], { httpOnly: true });
+  res.redirect("/semester/marks");
+});
+
+app.get("/semester/sem5", (req, res) => {
+  var subs = [
+    "18**51",
+    "18**52",
+    "18**53",
+    "18**54",
+    "18**55",
+    "18**56",
+    "18**L57",
+    "18**L58",
+    "Environmental Studies",
+  ];
+  res.cookie("subjects", ["5th Semester", subs], { httpOnly: true });
+  res.redirect("/semester/marks");
+});
+
+app.get("/semester/sem6", (req, res) => {
+  var subs = [
+    "18**61",
+    "18**62",
+    "18**63",
+    "18**64*",
+    "18**65*",
+    "18**L66",
+    "18**L67",
+    "18**MP68",
+  ];
+  res.cookie("subjects", ["6th Semester", subs], { httpOnly: true });
+  res.redirect("/semester/marks");
+});
+
+app.get("/semester/sem7", (req, res) => {
+  var subs = [
+    "18**71",
+    "18**72",
+    "18**73*",
+    "18**74*",
+    "18**75*",
+    "18**L76",
+    "18**L77",
+  ];
+  res.cookie("subjects", ["7th Semester", subs], { httpOnly: true });
+  res.redirect("/semester/marks");
+});
+
+app.get("/semester/sem8", (req, res) => {
+  var subs = ["18**81", "18**82*", "18**P83", "18**S84", "18**185"];
+  res.cookie("subjects", ["8th Semester", subs], { httpOnly: true });
+  res.redirect("/semester/marks");
+});
+
+// -------------------------------------------------------------------------------------------------------
+
 app.get("/cgpa", (req, res) => {
   res.render("cgpa", { title: "cgpa" });
 });
